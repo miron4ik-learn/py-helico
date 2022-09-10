c2099f5b7e91d55b02912b8b1185d8bc05105dc4
miron4ik <miron4ik.life@yandex.ru>
2022-09-10 19:19:08 +0300

    Implements mechanics of ticks

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import os
+import time
 from map import Map
 
-tmp = Map(20, 10)
+TICK_SLEEP = 0.05
 
-tmp.generate_forest(3, 10)
-tmp.generate_river(10)
-tmp.generate_river(10)
-tmp.generate_river(10)
+map = Map(20, 10)
+map.generate_forest(3, 10)
+map.generate_river(10)
+map.generate_river(10)
+map.generate_river(10)
+map.print_map()
 
-tmp.print_map()
+tick = 1
+while True:
+  os.system('cls')
+  
+  print('TICK', tick)
+  map.print_map()
+  
+  tick += 1
+  time.sleep(TICK_SLEEP)
